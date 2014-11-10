86a1340bb4678c8cc65ffa724aae006952182839
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-10 16:13:59 +0000

    Commented the regression test to elucidate its function

--- test_boids.py
+++ test_boids.py
@@ -3,10 +3,20 @@ from nose.tools import assert_almost_equal
 import os
 import yaml
 
+
+# A regression test to check the model behaves as it did previously
 def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
+
+    # Load the regression data from fixture.yaml
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+
+    # Take the input data from the regression_data["before"]
+    boid_data = regression_data["before"]
+
+    # Run the model
     update_boids(boid_data)
+
+    # Compare output of model boid_data with regression_data "after"
     for after,before in zip(regression_data["after"],boid_data):
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
