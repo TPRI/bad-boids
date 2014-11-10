89cfbba07c71b97396f4d65a7067decd3e6add17
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-10 20:11:29 +0000

    Converted the boids module into a Boids Class

--- boids.py
+++ boids.py
@@ -6,73 +6,55 @@ This code has been modified from a deliberately bad implementation of
 for use as an exercise on refactoring.
 """
 
-from matplotlib import pyplot as plt
-from matplotlib import animation
 import random
 import os
 import yaml
 
-# Load initial config from initial_config.yml
-INIT_CONFIG = yaml.load(open(os.path.join(os.path.dirname(__file__), 'initial_config.yml')))
+class Boids(object):
+    
+    def __init__(self):
+        
+        # Load initial config from initial_config.yml
+        INIT_CONFIG = yaml.load(open(os.path.join(os.path.dirname(__file__), 'initial_config.yml')))
+        
+        self.num_boids = INIT_CONFIG["num_boids"]
+        
+        # Construct boids as a array of arrays
+        boids_x = [random.uniform(INIT_CONFIG["x_min"], INIT_CONFIG["x_max"]) for x in range(self.num_boids)]
+        boids_y = [random.uniform(INIT_CONFIG["y_min"], INIT_CONFIG["y_max"]) for x in range(self.num_boids)]
+        boid_x_velocities = [random.uniform(INIT_CONFIG["vx_min"], INIT_CONFIG["vx_max"]) for x in range(self.num_boids)]
+        boid_y_velocities = [random.uniform(INIT_CONFIG["vy_min"], INIT_CONFIG["vy_max"]) for x in range(self.num_boids)]
+        self.boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
-NUM_BOIDS = INIT_CONFIG["num_boids"]
+    # The main simulation
+    def update_boids(self):
+    
+        xs, ys, xvs, yvs = self.boids
+    
+        # Fly towards the middle
+        for i in range(self.num_boids):
+            for j in range(self.num_boids):
+                xvs[i] += (xs[j] - xs[i]) * 0.01 / self.num_boids
+        for i in range(self.num_boids):
+            for j in range(self.num_boids):
+                yvs[i] += (ys[j] - ys[i]) * 0.01 / self.num_boids
+    
+        # Fly away from nearby boids
+        for i in range(self.num_boids):
+            for j in range(self.num_boids):
+                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
+                    xvs[i] = xvs[i] + (xs[i] - xs[j])
+                    yvs[i] = yvs[i] + (ys[i] - ys[j])
+    
+        # Try to match speed with nearby boids
+        for i in range(self.num_boids):
+            for j in range(self.num_boids):
+                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
+                    xvs[i] += (xvs[j] - xvs[i]) * 0.125 / self.num_boids
+                    yvs[i] += (yvs[j] - yvs[i]) * 0.125 / self.num_boids
+    
+        # Move according to velocities
+        for i in range(self.num_boids):
+            xs[i] = xs[i] + xvs[i]
+            ys[i] = ys[i] + yvs[i]
 
-# Construct boids as a array of arrays
-boids_x = [random.uniform(INIT_CONFIG["x_min"], INIT_CONFIG["x_min"]) for x in range(NUM_BOIDS)]
-boids_y = [random.uniform(INIT_CONFIG["y_min"], INIT_CONFIG["y_min"]) for x in range(NUM_BOIDS)]
-boid_x_velocities = [random.uniform(INIT_CONFIG["vx_min"], INIT_CONFIG["vx_min"]) for x in range(NUM_BOIDS)]
-boid_y_velocities = [random.uniform(INIT_CONFIG["vy_min"], INIT_CONFIG["vy_min"]) for x in range(NUM_BOIDS)]
-boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-
-
-# The main simulation
-def update_boids(boids):
-
-    xs, ys, xvs, yvs = boids
-
-    # Fly towards the middle
-    for i in range(NUM_BOIDS):
-        for j in range(NUM_BOIDS):
-            xvs[i] += (xs[j] - xs[i]) * 0.01 / NUM_BOIDS
-    for i in range(NUM_BOIDS):
-        for j in range(NUM_BOIDS):
-            yvs[i] += (ys[j] - ys[i]) * 0.01 / NUM_BOIDS
-
-    # Fly away from nearby boids
-    for i in range(NUM_BOIDS):
-        for j in range(NUM_BOIDS):
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
-                xvs[i] = xvs[i] + (xs[i] - xs[j])
-                yvs[i] = yvs[i] + (ys[i] - ys[j])
-
-    # Try to match speed with nearby boids
-    for i in range(NUM_BOIDS):
-        for j in range(NUM_BOIDS):
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
-                xvs[i] += (xvs[j] - xvs[i]) * 0.125 / NUM_BOIDS
-                yvs[i] += (yvs[j] - yvs[i]) * 0.125 / NUM_BOIDS
-
-    # Move according to velocities
-    for i in range(NUM_BOIDS):
-        xs[i] = xs[i] + xvs[i]
-        ys[i] = ys[i] + yvs[i]
-
-
-# Create a plot figure
-figure = plt.figure()
-axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
-
-
-# A function to run the simulation and update the plot
-def animate(frame):
-    update_boids(boids)
-    scatter.set_offsets(zip(boids[0], boids[1]))
-
-# Animate the plot
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
-
-# Run if module is called directly
-if __name__ == "__main__":
-    plt.show()
