65eb58d461de841155073639e72da7a9e7ed7581
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-10 16:21:10 +0000

    Commented the record_fixtures.py

--- record_fixture.py
+++ record_fixture.py
@@ -1,10 +1,20 @@
+#File to create regression test data
+
 import yaml
 import boids
 from copy import deepcopy
-before=deepcopy(boids.boids)
+
+# Create a deepcopy of the initial state of the boids object before simulation
+before = deepcopy(boids.boids)
+
+# Run the simulation
 boids.update_boids(boids.boids)
-after=boids.boids
-fixture={"before":before,"after":after}
-fixture_file=open("fixture.yml",'w')
+
+# Assign the result of the simulation to after
+after = boids.boids
+
+# Create a yaml file of with the before and after state
+fixture = {"before":before,"after":after}
+fixture_file = open("fixture.yml",'w')
 fixture_file.write(yaml.dump(fixture))
 fixture_file.close()
