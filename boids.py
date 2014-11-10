67958ec22601ba8eaa7cf2e2eb72805dce6af76b
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-10 16:49:04 +0000

    Commented boids.py and modified documentation

--- boids.py
+++ boids.py
@@ -1,5 +1,7 @@
 """
-A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
+A exercise in refactoring.
+
+This code has been modified from a deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
 for use as an exercise on refactoring.
 """
 
@@ -7,14 +9,14 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Deliberately terrible code for teaching purposes
-
+# Construct boids as a array of arrays
 boids_x=[random.uniform(-450,50.0) for x in range(50)]
 boids_y=[random.uniform(300.0,600.0) for x in range(50)]
 boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
 boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
+# The main simulation
 def update_boids(boids):
 	xs,ys,xvs,yvs=boids
 	# Fly towards the middle
@@ -42,17 +44,20 @@ def update_boids(boids):
 		ys[i]=ys[i]+yvs[i]
 
 
+# Create a plot figure
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
 scatter=axes.scatter(boids[0],boids[1])
 
+# A function to run the simulation and update the plot
 def animate(frame):
    update_boids(boids)
    scatter.set_offsets(zip(boids[0],boids[1]))
 
-
+# Animate the plot
 anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
 
+# Run if module is called directly
 if __name__ == "__main__":
     plt.show()
