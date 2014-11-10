41ff30802e3b0ec645f856c0d953685f6d3c6349
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-10 17:07:16 +0000

    Made test_boids.py PEP8 compliant

--- test_boids.py
+++ test_boids.py
@@ -6,9 +6,8 @@ import yaml
 
 # A regression test to check the model behaves as it did previously
 def test_bad_boids_regression():
-
     # Load the regression data from fixture.yaml
-    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
 
     # Take the input data from the regression_data["before"]
     boid_data = regression_data["before"]
@@ -17,7 +16,6 @@ def test_bad_boids_regression():
     update_boids(boid_data)
 
     # Compare output of model boid_data with regression_data "after"
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
-	
+    for after, before in zip(regression_data["after"], boid_data):
+        for after_value, before_value in zip(after, before):
+            assert_almost_equal(after_value, before_value, delta=0.01)
