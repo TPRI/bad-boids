dceded247038fe319edf80902e66e1b8e5efcf34
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-10 20:13:07 +0000

    Added a new module to create an instance of Boids, Run simulation and plot

---
+++ run_boids.py
@@ -0,0 +1,23 @@
+from boids import Boids
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+my_boids = Boids()
+
+# Create a plot figure
+figure = plt.figure()
+axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+scatter = axes.scatter(my_boids.boids[0], my_boids.boids[1])
+
+# A function to run the simulation and update the plot
+def animate(frame):
+    my_boids.update_boids()
+    scatter.set_offsets(zip(my_boids.boids[0], my_boids.boids[1]))
+
+# Animate the plot
+anim = animation.FuncAnimation(figure, animate,
+                               frames=50, interval=50)
+
+# Run if module is called directly
+if __name__ == "__main__":
+    plt.show()
