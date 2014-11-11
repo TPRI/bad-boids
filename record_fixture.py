978c958fe760e13114e783e8f41a3eb51ce7e18b
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-11 19:33:54 +0000

    Got regression test working again

--- record_fixture.py
+++ record_fixture.py
@@ -1,17 +1,19 @@
 # File to create regression test data
 
 import yaml
-import boids
+from boids import Boids
 from copy import deepcopy
 
+my_boids = Boids()
+
 # Create a deepcopy of the initial state of the boids object before simulation
-before = deepcopy(boids.boids)
+before = [my_boids.get_x_coordinates(), my_boids.get_y_coordinates(),my_boids.get_vx_coordinates(), my_boids.get_vy_coordinates()]
 
 # Run the simulation
-boids.update_boids(boids.boids)
+my_boids.update_boids()
 
 # Assign the result of the simulation to after
-after = boids.boids
+after = [my_boids.get_x_coordinates(), my_boids.get_y_coordinates(),my_boids.get_vx_coordinates(), my_boids.get_vy_coordinates()]
 
 # Create a yaml file of with the before and after state
 fixture = {"before": before, "after": after}
--- test_boids.py
+++ test_boids.py
@@ -1,19 +1,31 @@
-from boids import update_boids
+from boids import Boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
 
 
+
 # A regression test to check the model behaves as it did previously
 def test_bad_boids_regression():
+
+    my_boids = Boids()
+
     # Load the regression data from fixture.yaml
     regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
 
     # Take the input data from the regression_data["before"]
     boid_data = regression_data["before"]
 
+    # Set the boids to the before state
+    my_boids.set_boids(boid_data)
+
     # Run the model
-    update_boids(boid_data)
+    my_boids.update_boids()
+
+    boid_data  = [my_boids.get_x_coordinates(), my_boids.get_y_coordinates(), my_boids.get_vx_coordinates(), my_boids.get_vy_coordinates()]
+
+    print regression_data["after"]
+    print boid_data
 
     # Compare output of model boid_data with regression_data "after"
     for after, before in zip(regression_data["after"], boid_data):
