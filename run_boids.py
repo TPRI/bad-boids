c2b7110a0a2d843f2f36806a8d37c5860f84dc26
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-11 21:20:17 +0000

    Added plot properties to plot_config.yml

--- run_boids.py
+++ run_boids.py
@@ -1,12 +1,24 @@
 from boids import Boids
 from matplotlib import pyplot as plt
 from matplotlib import animation
+import yaml
+import os
 
+# Load plot config from plot_config.yml
+plot_config = yaml.load(open(os.path.join(os.path.dirname(__file__), 'plot_config.yml')))
+xlim_max = plot_config["xlim_max"]
+xlim_min = plot_config["xlim_min"]
+ylim_max = plot_config["ylim_max"]
+ylim_min = plot_config["ylim_min"]
+framesT = plot_config["frames"]
+intervalT = plot_config["interval"]
+
+# Create a Boid object (ie a flock of boids)
 my_boids = Boids()
 
 # Create a plot figure
 figure = plt.figure()
-axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+axes = plt.axes(xlim=(xlim_min, xlim_max), ylim=(ylim_min, ylim_max))
 scatter = axes.scatter(my_boids.get_boids()[0], my_boids.get_boids()[1])
 
 
@@ -16,8 +28,7 @@ def animate(frame):
     scatter.set_offsets(zip(my_boids.get_boids()[0], my_boids.get_boids()[1]))
 
 # Animate the plot
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+anim = animation.FuncAnimation(figure, animate, frames=framesT, interval=intervalT)
 
 # Run if module is called directly
 if __name__ == "__main__":
