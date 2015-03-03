06f1c84b23fc42c31c98489c266395f03ce95365
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2015-03-03 16:07:12 +0000

    Tidied up the creation of the numpy arrays

--- boids.py
+++ boids.py
@@ -24,18 +24,11 @@ class Boids(object):
         self.speed_match_condition = INIT_CONFIG["speed_match_condition"]
         self.speed_match_scale = INIT_CONFIG["speed_match_scale"]
         
-        # Construct boids as a array of arrays
-        boids_x = [random.uniform(INIT_CONFIG["x_min"], INIT_CONFIG["x_max"]) for x in range(self.num_boids)]
-        boids_y = [random.uniform(INIT_CONFIG["y_min"], INIT_CONFIG["y_max"]) for x in range(self.num_boids)]
-        boid_x_velocities = [random.uniform(INIT_CONFIG["vx_min"], INIT_CONFIG["vx_max"]) for x in range(self.num_boids)]
-        boid_y_velocities = [random.uniform(INIT_CONFIG["vy_min"], INIT_CONFIG["vy_max"]) for x in range(self.num_boids)]
-        self.boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-
-        # Convert to numpy arrays
-        boids_x = np.array(boids_x)
-        boids_y = np.array(boids_y)
-        boid_x_velocities = np.array(boid_x_velocities)
-        boid_y_velocities = np.array(boid_y_velocities)
+        # Construct boids as a array of numpy arrays
+        boids_x = np.array([random.uniform(INIT_CONFIG["x_min"], INIT_CONFIG["x_max"]) for x in range(self.num_boids)])
+        boids_y = np.array([random.uniform(INIT_CONFIG["y_min"], INIT_CONFIG["y_max"]) for x in range(self.num_boids)])
+        boid_x_velocities = np.array([random.uniform(INIT_CONFIG["vx_min"], INIT_CONFIG["vx_max"]) for x in range(self.num_boids)])
+        boid_y_velocities = np.array([random.uniform(INIT_CONFIG["vy_min"], INIT_CONFIG["vy_max"]) for x in range(self.num_boids)])
         self.boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
     # The main simulation
